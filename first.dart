8583402c82030850408829193f4eda18089079c3
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-03-23 14:52:52 +0600

    Initial commit

---
+++ first.dart
@@ -0,0 +1,85 @@
+//import 'dart:io';
+
+void main(){
+  print("Welcome");
+  //var name=stdin.readLineSync();
+ // stdout.write("Welcome to $name");
+
+ /*int a=5;
+ print(a);
+ BigInt LongA;
+ LongA=BigInt.parse("452349877123");
+ print(LongA);*/
+ /*num a=333;
+ print(a);*/
+
+ //Dynamic dattype....
+
+ /*var name;
+ name="Rahat";
+ name=7;
+ name=55.55;
+ print(name);*/
+
+/* var std=new Human();
+ std.man();
+ */
+
+// Function calling 
+ /*var mc=new Human();
+  mc.Man("Rahat");
+  //
+  //
+  //
+  mc.Man("Ahmed");
+  //
+  //
+  //
+  mc.Man("Shafi");
+  */
+
+ /* var obj=new Value();
+  print(obj.add());
+  */
+
+  /*var obj=new Value();
+  print(obj.add(8, 8));
+  */
+    
+}
+
+
+
+
+
+
+/*
+class Human{
+  void man(){
+    print("Hi,Mr.been.");
+  }
+  
+}*/
+/*
+class Human{
+  void Man(String name){
+    print(name);
+  }
+}*/
+/*class Value{
+  int add(){
+    int a,b,sum;
+    a=5;
+    b=5;
+    sum=a+b;
+    return sum;
+  }
+}*/
+
+/*
+class Value{
+  int add(int num1,int num2){
+    int sum=num1+num2;
+    return sum;
+  }
+}*/
